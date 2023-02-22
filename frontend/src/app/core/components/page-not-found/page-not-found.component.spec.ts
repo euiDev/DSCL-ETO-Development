a961c7cfd9bd8f6ffefe2cd316cbb845a57da127
Rhodejehan Pagarigan <rhodejehanpagarigan@MacBook-Pro.local>
2023-02-22 11:06:42 +0800

    Modified page not found & media queries

--- frontend/src/app/core/components/page-not-found/page-not-found.component.spec.ts
+++ frontend/src/app/core/components/page-not-found/page-not-found.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { PageNotFoundComponent } from './page-not-found.component';
 
@@ -8,9 +9,9 @@ describe('PageNotFoundComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PageNotFoundComponent ]
-    })
-    .compileComponents();
+      declarations: [PageNotFoundComponent],
+      imports: [RouterTestingModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(PageNotFoundComponent);
     component = fixture.componentInstance;
--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -14,6 +14,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [PageNotFoundComponent, LoginComponent, HeaderComponent],
